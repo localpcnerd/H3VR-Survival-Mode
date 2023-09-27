f65cb6da96c16715c2e904fde28dc6e558e40f99
local <77299433+localpcnerd@users.noreply.github.com>
2023-09-26 22:56:36 -0600

    Initial Scripts Upload

---
+++ Scripts/SosigWaveSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if H3VR_IMPORTED
+using FistVR;
+
+namespace localpcnerd.SurvivalMode
+{
+    public class SosigWaveSpawner : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
+#endif
